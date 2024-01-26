f183efe947749107513dc5a9dcede20615c17783
EliasR07 <eliasalejo01@gmail.com>
2024-01-26 17:41:12 -0400

    fix: moving firebase to env

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -7,13 +7,13 @@ export const init = () => {
   if (apps.length > 0) return;
 
   const firebaseConfig = {
-    apiKey: "AIzaSyC0b9E2BeKQIjJkyWG5aDz1ij2ytb3SqPU",
-    authDomain: "mmosh-app.firebaseapp.com",
-    projectId: "mmosh-app",
-    storageBucket: "mmosh-app.appspot.com",
-    messagingSenderId: "1055598871582",
-    appId: "1:1055598871582:web:6b34f891ede606e330da0f",
-    measurementId: "G-6N7WZNRFVT",
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
   };
 
   initializeApp(firebaseConfig);

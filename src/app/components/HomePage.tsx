ad3b8f8d0e6321124f1052e0898d905e1aa54f9a
EliasR07 <eliasalejo01@gmail.com>
2024-01-27 15:07:21 -0400

    fix: Adjusting some designs

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -52,7 +52,7 @@ const HomePage = () => {
 
   return (
     <div className="w-full h-screen flex flex-col items-center mt-6 home-content">
-      <div className="self-center md:max-w-[50%] xs:max-w-[90%]">
+      <div className="self-center md:max-w-[50%] max-w-[80%]">
         <p className="text-center text-white font-goudy font-normal mb-[3vmax] mt-[1vmax]">
           Welcome Home, {currentUser?.profile?.name}
         </p>

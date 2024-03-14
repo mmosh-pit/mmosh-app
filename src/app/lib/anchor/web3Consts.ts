1a351712de7dc71a3df1ab57f5bdd69d50c5ffdf
EliasR07 <eliasalejo01@gmail.com>
2024-03-14 10:59:07 -0400

    fix: Reverting changes on constants to point to environment variables

--- src/app/lib/anchor/web3Consts.ts
+++ src/app/lib/anchor/web3Consts.ts
@@ -12,23 +12,10 @@ export const web3Consts = {
     "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
   ),
   addressLookupTableProgram: web3.AddressLookupTableProgram.programId,
-  // oposToken: new web3.PublicKey(process.env.NEXT_PUBLIC_OPOS_TOKEN!),
-  // rootProfile: new web3.PublicKey(process.env.NEXT_PUBLIC_ROOT_PROFILE!),
-  // genesisProfile: new web3.PublicKey(process.env.NEXT_PUBLIC_GENESIS_PROFILE!),
-  // commonLut: new web3.PublicKey(process.env.NEXT_PUBLIC_COMMON_LUT!),
-
-  oposToken: new web3.PublicKey("6vgT7gxtF8Jdu7foPDZzdHxkwYFX9Y1jvgpxP8vH2Apw"),
-  rootProfile: new web3.PublicKey(
-    "85YaBFhbwuqPiRVNrXdMJwdt1qjdxbtypGcFBc6Tp7qA",
-  ),
-  genesisProfile: new web3.PublicKey(
-    "J2tFYfnM4t8XMcaMsR2WyUhaNrkJ26pwKZGq2J9T7rQL",
-  ),
-  commonLut: new web3.PublicKey("5D3XazMMpBcQb8FLxTWMB7WoWzU1YeVP3usekchp1xLu"),
-
-  badgeCollection: new web3.PublicKey("147m8aw1hcmyBduQvSJoXCFaLb8wQSe9DNxWVsLTfqec"),
-
-  rootCollection: new web3.PublicKey("9h9c4idX2UnGhFa266dQxMGD5kMk9pigumRAV2x9Hbk2"),
+  oposToken: new web3.PublicKey(process.env.NEXT_PUBLIC_OPOS_TOKEN!),
+  rootProfile: new web3.PublicKey(process.env.NEXT_PUBLIC_ROOT_PROFILE!),
+  genesisProfile: new web3.PublicKey(process.env.NEXT_PUBLIC_GENESIS_PROFILE!),
+  commonLut: new web3.PublicKey(process.env.NEXT_PUBLIC_COMMON_LUT!),
 
   LAMPORTS_PER_OPOS: 1000_000_000,
   Seeds: {

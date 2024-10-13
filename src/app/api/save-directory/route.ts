57c68b027a161bc96e10b899c033f7cfc5712f01
balajimara <balaji.jeyakumar@mara.com>
2024-10-14 01:25:10 +0530

    updating price issue to live

--- src/app/api/save-directory/route.ts
+++ src/app/api/save-directory/route.ts
@@ -6,11 +6,27 @@ export async function POST(req: NextRequest) {
   const directoryCollection = db.collection("mmosh-app-directory");
   const params = await req.json();
 
-  const mmoshUsdcPrice = await axios.get(
-    `https://price.jup.ag/v6/price?ids=MMOSH&vsToken=USDC`,
-  );
+  let usdcPrice;
+  
 
-  const USDCPrice = mmoshUsdcPrice.data?.data?.MMOSH?.price || 0;
+
+  if(params.basesymbol === "PTVB") {
+    let apiResponse  = await axios.get(
+      `https://price.jup.ag/v6/price?ids=PTVB&vsToken=USDC`,
+    );
+    usdcPrice = apiResponse.data?.data?.MMOSH?.price || 0.3;
+  } else if(params.basesymbol === "PTVR") {
+    let apiResponse  = await axios.get(
+      `https://price.jup.ag/v6/price?ids=PTVR&vsToken=USDC`,
+    );
+    usdcPrice = apiResponse.data?.data?.PTVR?.price || 0.003;
+  } else {
+    let apiResponse  = await axios.get(
+      `https://price.jup.ag/v6/price?ids=MMOSH&vsToken=USDC`,
+    );
+    usdcPrice = apiResponse.data?.data?.MMOSH?.price || 0.3;
+  }
+
 
   directoryCollection.insertOne({
     basekey: params.basekey,
@@ -23,7 +39,7 @@ export async function POST(req: NextRequest) {
     targetimg: params.targetimg,
     value: params.value,
     price: params.price,
-    usdcPrice: params.value * USDCPrice,
+    usdcPrice: params.value * usdcPrice,
     type: params.type,
     wallet: params.wallet,
     created_date: new Date(),

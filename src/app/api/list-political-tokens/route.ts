dff279b9fafd7fa8fcbe2018b54ee981dbee8c7f
EliasR07 <eliasalejo01@gmail.com>
2024-10-16 14:52:30 -0400

    ft: adding political memecoins listing function, as well as in swap's coin select

---
+++ src/app/api/list-political-tokens/route.ts
@@ -0,0 +1,40 @@
+import { db } from "../../lib/mongoClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const collection = db.collection("mmosh-app-project-tokens");
+
+  const { searchParams } = new URL(req.url);
+  const param = searchParams.get("search") as string;
+
+  if (param != "") {
+    let search = {
+      $or: [
+        {
+          name: {
+            $regex: new RegExp(param, "ig"),
+          },
+        },
+        {
+          symbol: {
+            $regex: new RegExp(param, "ig"),
+          },
+        },
+        {
+          token: {
+            $regex: new RegExp(param, "ig"),
+          },
+        },
+      ],
+    };
+    const result = await collection.find(search).limit(100).toArray();
+    return NextResponse.json([...result], {
+      status: 200,
+    });
+  } else {
+    const result = await collection.find().toArray();
+    return NextResponse.json([...result], {
+      status: 200,
+    });
+  }
+}
--- src/app/components/Swap/CoinSelect.tsx
+++ src/app/components/Swap/CoinSelect.tsx
@@ -29,91 +29,113 @@ const CoinSelect = ({
   isBase,
   readonly,
 }: Props) => {
-  const fetching = React.useRef(false);
-
   const [coinsList, setCoinsList] = React.useState<Coin[]>([]);
-  const [recentCoins, setRecentCoins] = React.useState<Coin[]>([]);
+
+  const [politicalCoins, setPoliticalCoins] = React.useState<Coin[]>([]);
+
+  // const [recentCoins, setRecentCoins] = React.useState<Coin[]>([]);
   const [searchText, setSearchText] = React.useState("");
 
-  const getCoinsList = async() => {
-
-    let newCoinList = []
-    fetching.current = true;
+  const getCoinsList = async () => {
+    let newCoinList = [];
     const listResult = await axios.get(`/api/list-tokens?search=${searchText}`);
-    fetching.current = false;
-    if(otherToken?.token === "") {
-      newCoinList = jupCoins
+
+    const politicalResult = await axios.get(
+      `/api/list-political-tokens?search=${searchText}`,
+    );
+
+    if (otherToken?.token === "") {
+      newCoinList = jupCoins;
       for (let index = 0; index < listResult.data.length; index++) {
         const element = listResult.data[index];
-        element.decimals = 9
-        element.iscoin = false
-        newCoinList.push(element)
+        element.decimals = 9;
+        element.iscoin = false;
+        newCoinList.push(element);
       }
     } else {
-      let memeCoin = listResult.data.filter((item: any) => item.token == otherToken?.token);
+      let memeCoin = listResult.data.filter(
+        (item: any) => item.token == otherToken?.token,
+      );
       if (memeCoin.length > 0) {
-        newCoinList.push(jupCoins[0]) // mmosh
-        newCoinList.push(jupCoins[1]) // ptvr
-        newCoinList.push(jupCoins[2]) // ptvb
+        newCoinList.push(jupCoins[0]); // mmosh
+        newCoinList.push(jupCoins[1]); // ptvr
+        newCoinList.push(jupCoins[2]); // ptvb
       }
-  
-      if(otherToken?.symbol.toLowerCase() == "mmosh") {
+
+      if (otherToken?.symbol.toLowerCase() == "mmosh") {
         let jupList = jupCoins;
         for (let index = 0; index < jupList.length; index++) {
           const element = jupList[index];
-          if(element.symbol != otherToken?.symbol) {
-            newCoinList.push(element)
+          if (element.symbol != otherToken?.symbol) {
+            newCoinList.push(element);
           }
         }
       }
- 
-      if(otherToken?.symbol.toLowerCase() == "ptvb" || otherToken?.symbol.toLowerCase() == "ptvr") {
-        newCoinList.push(jupCoins[0]) // mmosh
-      }
-  
-      if(otherToken?.symbol.toLowerCase() == "usdc") {
-        newCoinList.push(jupCoins[0]) // mmosh
-        newCoinList.push(jupCoins[4]) // sol
-      }
-  
-      if(otherToken?.symbol.toLowerCase() == "wsol") {
-        newCoinList.push(jupCoins[0]) // mmosh
-        newCoinList.push(jupCoins[3]) // usdc
+
+      if (
+        otherToken?.symbol.toLowerCase() == "ptvb" ||
+        otherToken?.symbol.toLowerCase() == "ptvr"
+      ) {
+        newCoinList.push(jupCoins[0]); // mmosh
       }
 
-      if(otherToken?.symbol.toLowerCase() == "mmosh" || otherToken?.symbol.toLowerCase() == "ptvr" || otherToken?.symbol.toLowerCase() == "ptvb") {
+      if (otherToken?.symbol.toLowerCase() == "usdc") {
+        newCoinList.push(jupCoins[0]); // mmosh
+        newCoinList.push(jupCoins[4]); // sol
+      }
+
+      if (otherToken?.symbol.toLowerCase() == "wsol") {
+        newCoinList.push(jupCoins[0]); // mmosh
+        newCoinList.push(jupCoins[3]); // usdc
+      }
+
+      if (
+        otherToken?.symbol.toLowerCase() == "mmosh" ||
+        otherToken?.symbol.toLowerCase() == "ptvr" ||
+        otherToken?.symbol.toLowerCase() == "ptvb"
+      ) {
         for (let index = 0; index < listResult.data.length; index++) {
           const element = listResult.data[index];
-          element.decimals = 9
-          element.iscoin = false
-          newCoinList.push(element)
+          element.decimals = 9;
+          element.iscoin = false;
+          newCoinList.push(element);
         }
-      } 
+      }
     }
 
-    if(searchText) {
+    if (searchText) {
       let finalList = newCoinList.filter((item: any) =>
         item.name.toLowerCase().includes(searchText.toLowerCase()),
-      )
+      );
       setCoinsList(finalList);
     } else {
       setCoinsList(newCoinList);
     }
+
+    setPoliticalCoins(politicalResult.data);
   };
 
   const handleTokenSelect = (token: Coin) => {
     onTokenSelect(token as SwapCoin, isBase);
-    (document.getElementById(isBase ? "coin_modal_base" : "coin_modal_target") as any)?.close();
-  }
+    (
+      document.getElementById(
+        isBase ? "coin_modal_base" : "coin_modal_target",
+      ) as any
+    )?.close();
+  };
 
   React.useEffect(() => {
     getCoinsList();
   }, [searchText, isBase]);
 
   const onOpenModel = async () => {
-     await getCoinsList();
-     (document.getElementById(isBase ? "coin_modal_base" : "coin_modal_target") as any)?.showModal()
-  }
+    await getCoinsList();
+    (
+      document.getElementById(
+        isBase ? "coin_modal_base" : "coin_modal_target",
+      ) as any
+    )?.showModal();
+  };
 
   return (
     <>
@@ -138,7 +160,10 @@ const CoinSelect = ({
           </>
         )}
       </div>
-      <dialog id={isBase ? "coin_modal_base" : "coin_modal_target"} className="modal">
+      <dialog
+        id={isBase ? "coin_modal_base" : "coin_modal_target"}
+        className="modal"
+      >
         <div className="flex flex-col modal-box w-[40%] bg-[#02001A] p-8">
           <div className="custom-select-open grow">
             <div className="flex w-full justify-between mb-2">
@@ -147,7 +172,11 @@ const CoinSelect = ({
               <button
                 className="cursor-pointer"
                 onClick={() =>
-                  (document.getElementById(isBase ? "coin_modal_base" : "coin_modal_target") as any)?.close()
+                  (
+                    document.getElementById(
+                      isBase ? "coin_modal_base" : "coin_modal_target",
+                    ) as any
+                  )?.close()
                 }
               >
                 <CloseIcon />
@@ -162,10 +191,31 @@ const CoinSelect = ({
               />
             </div>
 
-
             <div className="w-full h-[0.5px] bg-[#36357C] px-2 mb-8 mt-2" />
 
             {coinsList.map((coin) => {
+              return (
+                <div className="my-2">
+                  <CoinListItem
+                    token={coin.token}
+                    bonding={coin.bonding}
+                    name={coin.name}
+                    desc={coin.desc}
+                    creatorUsername={coin.creatorUsername}
+                    symbol={coin.symbol}
+                    image={coin.image}
+                    iscoin={coin.iscoin}
+                    decimals={coin.decimals}
+                    onTokenSelect={handleTokenSelect}
+                    key={coin.token}
+                  />
+                </div>
+              );
+            })}
+
+            <div className="w-full h-[0.5px] bg-[#36357C] px-2 mb-8 mt-2" />
+
+            {politicalCoins.map((coin) => {
               return (
                 <div className="my-2">
                   <CoinListItem

b2ed458efbe429b3bef45ec0ce48d69fd8cdc792
EliasR07 <eliasalejo01@gmail.com>
2024-10-23 12:56:58 -0400

    Increasing free ptv mint function timeout to 300s

--- src/app/api/ptv/free/route.ts
+++ src/app/api/ptv/free/route.ts
@@ -3,67 +3,76 @@ import { NextRequest, NextResponse } from "next/server";
 import * as anchor from "@coral-xyz/anchor";
 import { Connection, Keypair } from "@solana/web3.js";
 import bs58 from "bs58";
-import { Connectivity as CurveConn } from "@/anchor/curve/bonding";
 import { Connectivity as CommunityConn } from "@/anchor/community";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 import { web3Consts } from "@/anchor/web3Consts";
 
+export const maxDuration = 300;
+
 export async function POST(req: NextRequest) {
-    try {
-        
-        const collection = db.collection("mmosh-app-ptv");
-        const { name, symbol, url, gensis, lut, receiver, key } = await req.json();
+  try {
+    const collection = db.collection("mmosh-app-ptv");
+    const { name, symbol, url, gensis, lut, receiver, key } = await req.json();
 
+    const privateKey = process.env.PTV_WALLET!;
+    const private_buffer = bs58.decode(privateKey);
+    const private_arrray = new Uint8Array(
+      private_buffer.buffer,
+      private_buffer.byteOffset,
+      private_buffer.byteLength / Uint8Array.BYTES_PER_ELEMENT,
+    );
+    let ptvOwner = Keypair.fromSecretKey(private_arrray);
 
-        const privateKey = process.env.PTV_WALLET!;
-        const private_buffer = bs58.decode(privateKey);
-        const private_arrray = new Uint8Array(
-        private_buffer.buffer,
-        private_buffer.byteOffset,
-        private_buffer.byteLength / Uint8Array.BYTES_PER_ELEMENT,
-        );
-        let ptvOwner = Keypair.fromSecretKey(private_arrray)
+    let rpcUrl: any = process.env.NEXT_PUBLIC_SOLANA_CLUSTER;
+    let connection = new Connection(rpcUrl);
+    let wallet = new NodeWallet(ptvOwner);
 
-        let rpcUrl: any = process.env.NEXT_PUBLIC_SOLANA_CLUSTER;
-        let connection = new Connection(rpcUrl);
-        let wallet = new NodeWallet(ptvOwner);
+    console.log("wallet.publicKey.toBase58() ", wallet.publicKey.toBase58());
+    const env = new anchor.AnchorProvider(connection, wallet, {
+      preflightCommitment: "processed",
+    });
 
-        console.log("wallet.publicKey.toBase58() ", wallet.publicKey.toBase58())
-        const env = new anchor.AnchorProvider(connection, wallet, {
-        preflightCommitment: "processed",
-        });
-  
-        let projectConn: CommunityConn = new CommunityConn(env, web3Consts.programID, new anchor.web3.PublicKey(key));
+    let projectConn: CommunityConn = new CommunityConn(
+      env,
+      web3Consts.programID,
+      new anchor.web3.PublicKey(key),
+    );
 
-        let res = await projectConn.mintFreePass({
-            name: name,
-            symbol: symbol,
-            uriHash: url,
-            genesisProfile: gensis,
-            commonLut: lut
-        },receiver);
+    let res = await projectConn.mintFreePass(
+      {
+        name: name,
+        symbol: symbol,
+        uriHash: url,
+        genesisProfile: gensis,
+        commonLut: lut,
+      },
+      receiver,
+    );
 
-        if(res.Ok) {
-            return NextResponse.json(
-                {
-                    status: true, 
-                    message: "free mint completed"
-                }, 
-            { status: 200 });
-        }
-        NextResponse.json(
-            {
-                status: false, 
-                message: "Something went wrong"
-            }, 
-        { status: 200 });
-    } catch (error) {
-        console.log("error ", error)
-        return NextResponse.json(
-            {
-                status: false, 
-                message: "Something went wrong"
-            }, 
-        { status: 200 }); 
+    if (res.Ok) {
+      return NextResponse.json(
+        {
+          status: true,
+          message: "free mint completed",
+        },
+        { status: 200 },
+      );
     }
+    NextResponse.json(
+      {
+        status: false,
+        message: "Something went wrong",
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log("error ", error);
+    return NextResponse.json(
+      {
+        status: false,
+        message: "Something went wrong",
+      },
+      { status: 200 },
+    );
+  }
 }

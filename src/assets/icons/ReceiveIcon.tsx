3eb277148a8c7c4a0e7935d307d45400ec22f4fd
EliasR07 <eliasalejo01@gmail.com>
2024-11-18 09:53:30 -0400

    ft: removing options from header and adding them into the Bags/Wallet page

---
+++ src/assets/icons/ReceiveIcon.tsx
@@ -0,0 +1,19 @@
+type Props = React.SVGProps<SVGSVGElement>;
+
+const ReceiveIcon = (props: Props) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M10.3751 13.2904H4.05966L15.1667 2.18328L13.8155 0.832031L2.70841 11.9391V5.6237H0.791748V15.207H10.3751V13.2904Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export default ReceiveIcon;
---
+++ src/assets/icons/SendWalletIcon.tsx
@@ -0,0 +1,19 @@
+type Props = React.SVGProps<SVGSVGElement>;
+
+const SendWalletIcon = (props: Props) => (
+  <svg
+    width="20"
+    height="17"
+    viewBox="0 0 20 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M0.875 16.1654V10.4154L8.54167 8.4987L0.875 6.58203V0.832031L19.0833 8.4987L0.875 16.1654Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export default SendWalletIcon;

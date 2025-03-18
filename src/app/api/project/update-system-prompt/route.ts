2b873f9ff2c3f9114ac74dd52cb4ad4e51e5d35a
EliasR07 <eliasalejo01@gmail.com>
2025-03-18 09:31:49 -0400

    ft: Adding update to chat agents systemPrompt

--- src/app/api/project/update-system-prompt/route.ts
+++ src/app/api/project/update-system-prompt/route.ts
@@ -3,12 +3,24 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest) {
   const collection = db.collection("mmosh-app-project");
+  const chatCollection = db.collection("chats");
 
   const data = await req.json();
 
   await collection.updateOne(
     {
       key: data.project,
+    },
+    {
+      $set: {
+        system_prompt: data.systemPrompt,
+      },
+    },
+  );
+
+  await chatCollection.updateMany(
+    {
+      "chatAgent.key": data.project,
     },
     {
       $set: {

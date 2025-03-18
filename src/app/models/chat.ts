72aab2c8e5accf957352d8808f7d9d2ec1213a18
EliasR07 <eliasalejo01@gmail.com>
2025-03-18 09:26:06 -0400

    ft: Adding agent-specific chat selector

---
+++ src/app/models/chat.ts
@@ -0,0 +1,36 @@
+interface Participant {
+  id: string;
+  name: string;
+  type: string;
+  picture: string;
+}
+
+export interface Message {
+  id: string;
+  content: string;
+  sender_id: string;
+  type: string;
+  created_at: string;
+  sender: string;
+  is_loading: boolean;
+}
+
+interface Agent {
+  id: string;
+  name: string;
+  desc: string;
+  image: string;
+  key: string;
+  symbol: string;
+  systemPrompt: string;
+  creatorUsername: string;
+}
+
+export interface Chat {
+  id: string;
+  participants: Participant[];
+  messages: Message[];
+  deactivated: boolean;
+  chatAgent: Agent | null;
+  lastMessage: Message | null;
+}

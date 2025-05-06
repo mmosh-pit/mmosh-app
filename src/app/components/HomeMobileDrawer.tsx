a53780d5c23d6c3585ef8b7785a0188b687639f1
EliasR07 <eliasalejo01@gmail.com>
2025-05-06 13:17:49 -0400

    ft: Adding mobile drawer on landing page

---
+++ src/app/components/HomeMobileDrawer.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+import { useAtom } from "jotai";
+
+import HamburgerIcon from "@/assets/icons/HamburgerIcon";
+import { isDrawerOpen } from "@/app/store";
+
+type Props = {
+  belowHeroRef: React.RefObject<HTMLDivElement>;
+  howItWorksSection: React.RefObject<HTMLDivElement>;
+  whySection: React.RefObject<HTMLDivElement>;
+  founderSection: React.RefObject<HTMLDivElement>;
+  testimonialsSection: React.RefObject<HTMLDivElement>;
+  safeSection: React.RefObject<HTMLDivElement>;
+};
+
+const HomeMobileDrawer = ({
+  belowHeroRef,
+  howItWorksSection,
+  whySection,
+  founderSection,
+  testimonialsSection,
+  safeSection,
+}: Props) => {
+  const [_, setIsDrawerOpen] = useAtom(isDrawerOpen);
+
+  return (
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label
+          htmlFor="my-drawer"
+          className="btn drawer-button"
+          onClick={() => setIsDrawerOpen(true)}
+        >
+          <HamburgerIcon />
+        </label>
+      </div>
+
+      <div className="drawer-side z-10">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+          onClick={() => setIsDrawerOpen(false)}
+        ></label>
+        <div className="flex flex-col menu p-4 w-80 min-h-full bg-[#09073A] text-base-content">
+          <div className="flex flex-col">
+            <a
+              className="text-base text-white cursor-pointer"
+              onClick={() =>
+                belowHeroRef.current?.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              Overview
+            </a>
+
+            <div className="lg:my-4 md:my-2" />
+
+            <a
+              className="text-base text-white cursor-pointer"
+              onClick={() =>
+                howItWorksSection.current?.scrollIntoView({
+                  behavior: "smooth",
+                })
+              }
+            >
+              How it Works
+            </a>
+
+            <div className="lg:my-4 md:my-2" />
+
+            <a
+              className="text-base text-white cursor-pointer"
+              onClick={() =>
+                whySection.current?.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              Why Join?
+            </a>
+
+            <div className="lg:my-4 md:my-2" />
+
+            <a
+              className="text-base text-white cursor-pointer"
+              onClick={() =>
+                founderSection.current?.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              From our Founder
+            </a>
+
+            <div className="lg:my-4 md:my-2" />
+            <a
+              className="text-base text-white cursor-pointer"
+              onClick={() =>
+                testimonialsSection.current?.scrollIntoView({
+                  behavior: "smooth",
+                })
+              }
+            >
+              Testimonials
+            </a>
+
+            <div className="lg:my-4 md:my-2" />
+            <a
+              className="text-base text-white cursor-pointer"
+              onClick={() =>
+                safeSection.current?.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              Safety
+            </a>
+          </div>
+
+          <div className="h-[1px] w-[90%] bg-white mt-4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeMobileDrawer;

b2124188ee73c9d0e8ec17c6de645f21cd7e6fab
AASHIK200227 <aashik200227@gmail.com>
2025-07-08 12:07:08 +0530

    updated changes related launchpad page

---
+++ src/app/launchpad/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+interface LaunchpadProps {
+
+}
+export const Launchpad = (props: LaunchpadProps) => {
+    return (
+        <div>
+            <h1>Next Launch</h1>
+        </div>
+    ) 
+}

184e1a0e111a86fdfc6e84a0de2b48320098e116
EliasR07 <eliasalejo01@gmail.com>
2025-07-15 10:19:28 -0400

    Removing duplicated message

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -89,7 +89,7 @@ const Header = () => {
   const [community] = useAtom(currentGroupCommunity);
 
   const param = searchParams.get("refer");
-   const [membershipStatus, setMembershipStatus] = React.useState("na")
+  const [membershipStatus, setMembershipStatus] = React.useState("na");
 
   React.useEffect(() => {
     if (param) {
@@ -437,7 +437,7 @@ const Header = () => {
     )
       return;
     fetchAllBalances();
-    checkMembershipStatus()
+    checkMembershipStatus();
   }, [wallet]);
 
   React.useEffect(() => {
@@ -495,10 +495,12 @@ const Header = () => {
     return <></>;
   }
 
-  const checkMembershipStatus = async()=> {
-    let membershipInfo = await axios.get("/api/membership/has-membership?wallet=" + wallet!.publicKey.toBase58());
-    setMembershipStatus(membershipInfo.data)
-  }
+  const checkMembershipStatus = async () => {
+    let membershipInfo = await axios.get(
+      "/api/membership/has-membership?wallet=" + wallet!.publicKey.toBase58(),
+    );
+    setMembershipStatus(membershipInfo.data);
+  };
 
   return (
     <>
@@ -717,28 +719,22 @@ const Header = () => {
               <p className="text-base my-4">{community.description}</p>
             </div>
           </div>
-
         )}
-      {pathname !== "/" && (
-        <MessageBanner
-          type="info"
-          message="This is a pre-release system for test purposes only. Do not rely on any information you see here. If you use crypto, you might lose all your money."
-        />
-      )}
-      {membershipStatus === "expired" &&
-        <div className="cursor-pointer" onClick={()=>{
-           router.push("/create/profile");
-        }}>
-          <MessageBanner
-            type="error"
-            message="Your membership is expired. pls upgrade"
-          />
-        </div>
-      }
+        {membershipStatus === "expired" && (
+          <div
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/create/profile");
+            }}
+          >
+            <MessageBanner
+              type="error"
+              message="Your membership is expired. pls upgrade"
+            />
+          </div>
+        )}
       </header>
     </>
-
-
   );
 };
 

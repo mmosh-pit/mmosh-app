8eb0348c670f6418cfde3e2874549f9194d0afb7
AASHIK200227 <aashik200227@gmail.com>
2025-07-22 18:10:59 +0530

    updated changes related to the member info api

---
+++ src/app/api/membership/get-membership-info/route.tsx
@@ -0,0 +1,29 @@
+
+import { db } from "@/app/lib/mongoClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const collection = db.collection("mmosh-app-user-membership");
+
+  const { searchParams } = new URL(req.url);
+  const wallet = searchParams.get("wallet");
+
+  const membershipInfo = await collection.find({
+    wallet: wallet,
+  }).sort("-created_Data").limit(1).toArray();
+
+  if (membershipInfo.length == 0) {
+    return NextResponse.json({}, { status: 200 });
+  }
+
+  let membershipData = membershipInfo[0]
+  console.log("membershipData", membershipData);
+  const now = new Date();
+  const expiry = new Date(membershipData.expirydate);
+
+  if (expiry < now) {
+    return NextResponse.json({}, { status: 200 });
+  } else {
+    return NextResponse.json(membershipInfo[0], { status: 200 });
+  }
+}

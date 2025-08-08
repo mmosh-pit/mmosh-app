ebfd5d770df2257fe9ddac4ef1dc874d07886676
EliasR07 <eliasalejo01@gmail.com>
2025-08-08 11:43:42 -0400

    ft: Updating bot directory/index with new designs

--- src/app/components/Project/ProjectCard.tsx
+++ src/app/components/Project/ProjectCard.tsx
@@ -6,13 +6,17 @@ import BotIcon from "@/assets/icons/BotIcon";
 import PersonIcon from "@/assets/icons/PersonIcon";
 import CoinIcon from "@/assets/icons/CoinIcon";
 
+function capitalizeFirstLetter(val: string) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
 const ProjectCard = (projectData: any) => {
   const router = useRouter();
   if (!projectData) return <></>;
 
   return (
     <div
-      className="flex bg-[#030007] bg-opacity-40 px-4 py-2 rounded-2xl border-[1px] border-[#353485] cursor-pointer"
+      className="flex bg-[#1E1D56] bg-opacity-40 px-6 py-4 rounded-3xl cursor-pointer"
       onClick={() => {
         router.push(
           `${process.env.NEXT_PUBLIC_APP_MAIN_URL}/projects/${projectData.data.symbol.toLowerCase()}`,
@@ -30,33 +34,16 @@ const ProjectCard = (projectData: any) => {
         </div>
       </div>
 
-      <div className="w-full flex flex-col justify-between">
+      <div className="w-full flex flex-col justify-between pt-2 pl-2">
         <div className="flex items-center">
-          <BotIcon />
-          <p className="text-white 2xl:text-lg text-base underline ml-2">
+          <p className="text-white 2xl:text-lg text-base underline mr-2">
             {" "}
             <span className="font-bold text-white 2xl:text-lg text-base">
               {projectData.data.name}
             </span>{" "}
             • {projectData.data.symbol}
           </p>
-        </div>
-
-        <div className="flex items-center">
-          <PersonIcon />
-          <a
-            className="2xl:text-base text-sm text-[#FF00C7] underline ml-2"
-            href={`${process.env.NEXT_PUBLIC_APP_MAIN_URL}/${projectData.data.creatorUsername}`}
-          >
-            @{projectData.data.creatorUsername}
-          </a>
-
-          <p className="2xl:text-lg text-base text-white mx-2"> • </p>
-
-          <CoinIcon />
-          <p className="2xl:text-base text-sm underline uppercase ml-2">
-            {projectData.data.creatorUsername}
-          </p>
+          <BotIcon />
         </div>
 
         <div className="2xl:my-4 my-3">
@@ -65,33 +52,29 @@ const ProjectCard = (projectData: any) => {
           </p>
         </div>
 
-        <div className="flex flex-col 2xl:mt-4 mt-2">
-          <div className="flex items-center rounded-lg px-2">
-            <p className="2xl:text-sm text-xs text-white">
-              <span className="font-bold text-sm text-white mr-4">
-                Subscribers
+        <div className="w-full flex justify-between items-end">
+          <div className="flex">
+            <p className="text-sm">
+              By{" "}
+              <span className="text-sm underline">
+                {projectData.data.creatorUsername}
+              </span>{" "}
+              •{" "}
+              <span className="text-sm underline">
+                {capitalizeFirstLetter(
+                  projectData.data.privacy != null &&
+                    projectData.data.privacy !== ""
+                    ? projectData.data.privacy
+                    : "private",
+                )}
               </span>
             </p>
-            <div className="px-2 bg-[#19066B] rounded-lg">
-              <p className="text-sm text-white">250</p>
-            </div>
           </div>
 
-          <div className="flex items-center rounded-lg px-2 mt-2">
-            <p className="2xl:text-sm text-xs text-white">
-              <span className="font-bold text-sm text-white mr-4">
-                Market Cap
-              </span>
-            </p>
-            <div className="px-2 bg-[#19066B] rounded-lg">
-              <p className="2xl:text-sm text-xs text-white">123 USDC</p>
-            </div>
-          </div>
+          <button className="self-start rounded-full h-8 bg-[#FF00AE] flex justify-center items-center px-4 py-2">
+            <p className="2xl:text-base text-sm text-white">Activate</p>
+          </button>
         </div>
-      </div>
-
-      <div className="self-start rounded-full w-[20%] h-8 bg-[#FF00AE] flex justify-center items-center">
-        <p className="2xl:text-base text-sm text-white">Activate</p>
       </div>
     </div>
   );

47b00ba2cb6f71177c48de79763dc6297ec55f89
sucildoss <sucil.jaya@gmail.com>
2025-09-23 20:17:50 +0530

    updated changes related to the setting screen notification message and get-elders API error handling

--- src/app/api/get-elders/route.ts
+++ src/app/api/get-elders/route.ts
@@ -2,20 +2,38 @@ import { db } from "../../lib/mongoClient";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const collection = db.collection("mmosh-app-lineage");
+  try {
+    const collection = db.collection("mmosh-app-lineage");
 
-  const { searchParams } = new URL(req.url);
-  const param = searchParams.get("profile");
+    const { searchParams } = new URL(req.url);
+    const param = searchParams.get("profile");
 
-  if (!param) {
-    return NextResponse.json("Invalid Request", { status: 400 });
+    if (!param) {
+      return NextResponse.json("Invalid Request", { status: 400 });
+    }
+
+    const res = await collection.findOne({
+      profile: param,
+    });
+    console.log(res,"res =============================>")
+    return NextResponse.json(
+      {
+        status: res?.lineage !== undefined,
+        result: res?.lineage !== undefined ? res?.lineage : {},
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        status: false,
+        result: {},
+      },
+      {
+        status: 200,
+      }
+    );
   }
-
-  const res = await collection.findOne({
-    profile: param,
-  });
-
-  return NextResponse.json(res?.lineage, {
-    status: 200,
-  });
 }

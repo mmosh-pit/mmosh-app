421a0909cf7c525cae1e7fdfd72172c10aa9590a
aashik <aashik200227@gmail.com>
2025-09-29 14:42:11 +0530

    launch pad design

--- src/app/components/LaunchPad/LaunchPad.tsx
+++ src/app/components/LaunchPad/LaunchPad.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import BigNumber from 'bignumber.js';
+import BigNumber from "bignumber.js";
 
 interface LaunchPadProps {
   getCountDownValues: any;
@@ -15,16 +15,24 @@ export const LaunchPad = (props: LaunchPadProps) => {
     { label: "Hours" },
     { label: "Minutes" },
     { label: "Seconds" },
-  ])
+  ]);
   const [amount, setAmount] = React.useState("");
   const [token, setToken] = React.useState(0);
   const [currentTranche, setCurrentTranche] = React.useState(0);
 
-  const handleAmount = (event: React.ChangeEvent<HTMLInputElement>, tokenInfo: any) => {
+  const handleAmount = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    tokenInfo: any
+  ) => {
     setAmount(event.target.value);
-    const calculatedToken = ((Number(event.target.value) / tokenInfo.presaleDetail.launchPrice) + Number(event.target.value) * Number(tokenInfo.presaleDetail.discount[currentTranche].percentage) / 100).toFixed(9)
+    const calculatedToken = (
+      Number(event.target.value) / tokenInfo.presaleDetail.launchPrice +
+      (Number(event.target.value) *
+        Number(tokenInfo.presaleDetail.discount[currentTranche].percentage)) /
+        100
+    ).toFixed(9);
     setToken(Number(calculatedToken));
-  }
+  };
   const getTrancheLabel = (index: number) => {
     const labels = ["1st Tranche", "2nd Tranche", "3rd Tranche", "4th Tranche"];
     if (currentTranche !== index) {
@@ -39,123 +47,200 @@ export const LaunchPad = (props: LaunchPadProps) => {
       available += Number(element.value);
       if (available - data.presaleDetail.totalSold > 0) {
         return (
-          <div className="w-[269px] flex flex-col items-start justify-center ml-[10px] text-white font-['Avenir LT Std',sans-serif]">
+          <div className=" flex flex-col items-start mt-5 ml-[10px] text-white font-['Avenir LT Std',sans-serif]">
             <div className="font-black text-[12px] leading-[1] mb-[8px] tracking-[-0.04em]">
               {getTrancheLabel(index)}
             </div>
             <div className="flex gap-[10px] mb-[6px]">
-              <div key={index} className="w-[50px] h-[27px] rounded-[4px] bg-[#3C39BE33] border-b border-[#fff3] flex flex-col items-center justify-center">
+              <div
+                key={index}
+                className="w-[50px] h-[27px] rounded-[4px] bg-[#3C39BE33]  flex flex-col items-center justify-center"
+              >
                 <div className="text-[9px] font-bold">Available</div>
-                <div className="text-[8px] font-light">{available - data.presaleDetail.totalSold}</div>
+                <div className="text-[8px] font-light">
+                  {available - data.presaleDetail.totalSold}
+                </div>
               </div>
-              <div key={index} className="w-[50px] h-[27px] rounded-[4px] bg-[#3C39BE33] border-b border-[#fff3] flex flex-col items-center justify-center">
+              <div
+                key={index}
+                className="w-[50px] h-[27px] rounded-[4px] bg-[#3C39BE33]  flex flex-col items-center justify-center"
+              >
                 <div className="text-[9px] font-bold">Purchased</div>
-                <div className="text-[8px] font-light">{new BigNumber(Number(element.value)).minus(new BigNumber(available - data.presaleDetail.totalSold)).toString()}</div>
+                <div className="text-[8px] font-light">
+                  {new BigNumber(Number(element.value))
+                    .minus(
+                      new BigNumber(available - data.presaleDetail.totalSold)
+                    )
+                    .toString()}
+                </div>
               </div>
-              <div key={index} className="w-[50px] h-[27px] rounded-[4px] bg-[#3C39BE33] border-b border-[#fff3] flex flex-col items-center justify-center">
+              <div
+                key={index}
+                className="w-[50px] h-[27px] rounded-[4px] bg-[#3C39BE33] flex flex-col items-center justify-center"
+              >
                 <div className="text-[9px] font-bold">Discount</div>
-                <div className="text-[8px] font-light">{element.percentage}%</div>
+                <div className="text-[8px] font-light">
+                  {element.percentage}%
+                </div>
               </div>
             </div>
             <div className="flex gap-[5px]">
-              <div key={index} className="w-[85px] h-[27px] rounded-[4px] bg-[#3C39BE33] border-b border-[#fff3] flex flex-col items-center justify-center text-center px-1">
-                <div className="text-[8.5px] font-bold leading-tight">Max Supply</div>
-                <div className="text-[8px] font-light">{data.coinDetail.supply} {data.coinDetail.symbol}</div>
+              <div
+                key={index}
+                className="w-[85px] h-[27px] rounded-[4px] bg-[#3C39BE33]  flex flex-col items-center justify-center text-center px-1"
+              >
+                <div className="text-[8.5px] font-bold leading-tight">
+                  Max Supply
+                </div>
+                <div className="text-[8px] font-light">
+                  {data.coinDetail.supply} {data.coinDetail.symbol}
+                </div>
               </div>
-              <div key={index} className="w-[85px] h-[27px] rounded-[4px] bg-[#3C39BE33] border-b border-[#fff3] flex flex-col items-center justify-center text-center px-1">
-                <div className="text-[8.5px] font-bold leading-tight">Launch Price</div>
-                <div className="text-[8px] font-light">{data.presaleDetail.launchPrice} USDC</div>
+              <div
+                key={index}
+                className="w-[85px] h-[27px] rounded-[4px] bg-[#3C39BE33]  flex flex-col items-center justify-center text-center px-1"
+              >
+                <div className="text-[8.5px] font-bold leading-tight">
+                  Launch Price
+                </div>
+                <div className="text-[8px] font-light">
+                  {data.presaleDetail.launchPrice} USDC
+                </div>
               </div>
-              <div key={index} className="w-[85px] h-[27px] rounded-[4px] bg-[#3C39BE33] border-b border-[#fff3] flex flex-col items-center justify-center text-center px-1">
-                <div className="text-[8.5px] font-bold leading-tight">Market Cap</div>
-                <div className="text-[8px] font-light">{data.presaleDetail.launchMarketCap} USDC</div>
+              <div
+                key={index}
+                className="w-[85px] h-[27px] rounded-[4px] bg-[#3C39BE33]  flex flex-col items-center justify-center text-center px-1"
+              >
+                <div className="text-[8.5px] font-bold leading-tight">
+                  Market Cap
+                </div>
+                <div className="text-[8px] font-light">
+                  {data.presaleDetail.launchMarketCap} USDC
+                </div>
               </div>
             </div>
           </div>
-        )
+        );
       }
     }
-  }
+  };
   const formatAmount = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
       maximumFractionDigits: 9,
     }).format(amount);
   };
   return (
     <>
-        <div className="relative">
-          {/* Header */}
-          <div className="h-[40px] w-[392px] bg-[#0A34C4] rounded-t-[14px] pl-[80px] pr-3 flex items-center justify-between relative">
-            <div className="flex flex-col font-poppins min-w-0">
-              <div className="text-[10px] font-medium underline whitespace-nowrap truncate">
-                {presale.coinDetail.name} <span className="text-[#C2C2C2]">• {presale.coinDetail.symbol}</span>
+      <div className="relative">
+        {/* Header */}
+        <div
+          className="flex flex-col lg:flex-row rounded-t-[14px] py-2 px-1 
+    lg:justify-between bg-gradient-to-r from-[#05195E] to-[#0A34C4]"
+        >
+          {/* Left section */}
+          <div className="w-[75px] h-[75px] rounded-full border-[2px] border-[#040432] overflow-hidden bg-white shadow-md self-center lg:hidden">
+            <img
+              src={presale.coinDetail.image}
+              alt="Token"
+              className="w-full h-full object-cover "
+            />
+          </div>
+          <div className="lg:ml-20 self-center">
+            <div className="flex items-center">
+              <div className="text-md text-white font-medium underline">
+                {presale.coinDetail.name}{" "}
               </div>
-              <div className="text-[9px] text-[#3C99FF] underline cursor-pointer whitespace-nowrap truncate">
-                {creator}
-              </div>
+              <span className="text-[#C2C2C2] ml-1">
+                • {presale.coinDetail.symbol}
+              </span>
             </div>
-            <div className="flex gap-3">
-              {timerData.map((item, i) => (
-                <div key={i} className="text-center">
-                  <div className="text-base font-bold text-white">{getCountDownValues(new Date(presale.presaleDetail.lockPeriod).getTime() - new Date().getTime(), item.label)}</div>
+            <div className="text-[9px] text-[#3C99FF] underline">{creator}</div>
+          </div>
+
+          {/* Right section */}
+          <div className="flex justify-center lg:justify-start mt-2 lg:mt-0 px-1.5 py-1 rounded-xl bg-gradient-to-r from-[#1C1A584D] to-[#3C39BE4D]">
+            {timerData.map((item, i) => (
+              <React.Fragment key={i}>
+                <div className="text-center px-2">
+                  <div className="text-base text-white font-goudy">
+                    {getCountDownValues(
+                      new Date(presale.presaleDetail.lockPeriod).getTime() -
+                        new Date().getTime(),
+                      item.label
+                    )}
+                  </div>
                   <div className="text-[9px] text-white/70">{item.label}</div>
                 </div>
-              ))}
-            </div>
-          </div>
 
-          {/* Body */}
-          <div className="w-[392px] h-[173px] bg-[#1A184D] border border-[#5a84ff] rounded-b-[14px] flex p-1 font-poppins">
-            {/* Left Side */}
-            <div className="w-[135px] flex flex-col pt-[40px] pr-3 relative">
-              <div className="absolute top-[-35px] left-[5px] z-10">
-                <div className="w-[75px] h-[75px] rounded-full border-[2px] border-[#040432] overflow-hidden bg-white shadow-md">
-                  <img
-                    src={presale.coinDetail.image}
-                    alt="Token"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-              </div>
-
-              <label className="text-[9px] font-extrabold mb-[2px] leading-none tracking-tight text-white">
-                Amount
-              </label>
-              <div className="flex items-center mb-[5px]">
-                <input
-                  type="number"
-                  placeholder="0.00"
-                  className="w-[70px] h-[24px] px-2 rounded-[4px] border border-[#FF8FE7] bg-[rgba(255,143,231,0.08)] text-white text-[10px] font-normal outline-none pr-1 appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-                  value={amount}
-                  onChange={(event) => handleAmount(event, presale)}
-                />
-                <span className="ml-[4px] text-[7px] text-white">USDC</span>
-              </div>
-
-              <div className="flex items-right text-white text-[10px] font-normal mb-[3px]">
-                <span className="text-grey">{formatAmount(token).replace(/[$,]/g, '').replace(/\.00$/, '')}</span>
-                <span className="ml-[34px] text-[#CFCFCF] text-[8px]">{presale.coinDetail.symbol}</span>
-              </div>
-
-              <div className="text-[#CFCFCF] text-[5.4px] leading-[110%] mb-[2px]">
-                Minimum: {presale.presaleDetail.purchaseMinimum} USDC<br />
-                Maximum: {presale.presaleDetail.purchaseMaximum} USDC
-              </div>
-              <button className="w-[70px] h-[21px] bg-[#FF00AE]/70 hover:bg-[#FF00AE] text-white rounded-[3px] text-[10px] font-bold0" onClick={() => buyToken(amount, token)}>
-                {isBuying ? "Buying" : "Buy"}
-              </button>
-
-              <p className="text-[6px] text-white mt-1 leading-[1.2]">
-                Plus a small amount of SOL for gas fees
-              </p>
-            </div>
-
-            {/* Right Side */}
-            {renderTranche(presale)}
+                {i !== timerData.length - 1 && (
+                  <div className="w-[1px] h-6 bg-white/30 self-center"></div>
+                )}
+              </React.Fragment>
+            ))}
           </div>
         </div>
+
+        {/* Body */}
+        <div className="  bg-[#1A184D] rounded-b-xl lg:flex lg:justify-between px-2 py-1 font-poppins shadow-sm shadow-blue-900 ">
+          {/* Left Side */}
+          <div className="  pt-5  relative">
+            <div className="absolute top-[-60px] lg:block hidden z-10">
+              <div className="w-[75px] h-[75px] rounded-full border-[2px] border-[#040432] overflow-hidden bg-white shadow-md">
+                <img
+                  src={presale.coinDetail.image}
+                  alt="Token"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+
+            <label className="text-[9px] font-extrabold mb-[2px] leading-none tracking-tight text-white">
+              Amount
+            </label>
+            <div className="flex justify-between items-center mb-[5px]">
+              <input
+                type="number"
+                placeholder="0.00"
+                className="lg:w-[70px] h-[24px] max-w-100 px-2 rounded-[4px] border border-[#FF8FE7] bg-[rgba(255,143,231,0.08)] text-white text-[10px] font-normal outline-none pr-1 appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                value={amount}
+                onChange={(event) => handleAmount(event, presale)}
+              />
+              <span className="ml-[4px] text-[7px] text-white">USDC</span>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="text-white w-[80px] max-w-[380px] overflow-hidden whitespace-nowrap truncate">
+                {formatAmount(token).replace(/[$,]/g, "").replace(/\.00$/, "")}
+              </div>
+              <div className="text-white text-[8px]">
+                {presale.coinDetail.symbol}
+              </div>
+            </div>
+
+            <div className="text-[#CFCFCF] text-[5.4px] mb-[2px]">
+              Minimum: {presale.presaleDetail.purchaseMinimum} USDC
+              <br />
+              Maximum: {presale.presaleDetail.purchaseMaximum} USDC
+            </div>
+
+            <div
+              className="px-2 py-1 bg-[#FF00AE] text-white rounded-[3px] text-[10px] font-bold m-auto text-center mx-3"
+              onClick={() => buyToken(amount, token)}
+            >
+              {isBuying ? "Buying" : "Buy"}
+            </div>
+
+            <p className="text-[0.281rem] text-white mt-1 text-center">
+              Plus a small amount of SOL for gas fees
+            </p>
+          </div>
+
+          {/* Right Side */}
+          {renderTranche(presale)}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};

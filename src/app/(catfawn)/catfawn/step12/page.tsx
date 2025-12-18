3598796a1870a8104f4edc80a89f9d46ac133494
AASHIK200227 <aashik200227@gmail.com>
2025-12-18 16:51:17 +0530

    Catfawn - Design Changes

--- src/app/(catfawn)/catfawn/step12/page.tsx
+++ src/app/(catfawn)/catfawn/step12/page.tsx
@@ -112,7 +112,7 @@ export default function Step12VC() {
           JSON.stringify({
             ...cachedData,
             currentStep: "catfawn/step13",
-            isMobileNumberVerified:true,
+            isMobileNumberVerified: true,
             completedSteps: 24,
           })
         );

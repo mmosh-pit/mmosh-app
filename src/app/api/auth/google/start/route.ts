46a29c0b6cc3c7285fc33868996de91501487b43
AASHIK200227 <aashik200227@gmail.com>
2026-01-12 20:44:41 +0530

    Updates for google calendar

--- src/app/api/auth/google/start/route.ts
+++ src/app/api/auth/google/start/route.ts
@@ -26,7 +26,8 @@ export async function GET(req: NextRequest) {
       "https://www.googleapis.com/auth/calendar.events",
       "https://www.googleapis.com/auth/drive.file",
       "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/userinfo.profile"
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/calendar.readonly",
     ].join(" "),
     state
   });
